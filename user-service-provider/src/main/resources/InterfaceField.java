0d33344b58d656b4fdfbbb3a2609e9004ff4c39a
derrickwu1984 <derrickwu@sina.com>
2019-06-19 17:23:18 +0800

    define bean

---
+++ user-service-provider/src/main/resources/InterfaceField.java
@@ -0,0 +1,15 @@
+
+public class InterfaceField {
+	private String id;
+	private String trs_name;
+	private String trs_code;
+	private String func_desc;
+	private String flag;
+	private String eng_name;
+	private String chinese_name;
+	private String data_type;
+	private String required;
+	private String remark;
+	
+
+}
